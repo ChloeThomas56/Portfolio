181bc3094a4413204ef1eeeef50e082014fa221a
ChloeThomas <chloe.thomas.92@gmail.com>
2024-11-19 14:23:43 +0100

    edit projects and theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import styles from './home.module.scss';
 import { MdArrowOutward } from 'react-icons/md';
+import { TfiWorld } from "react-icons/tfi";
 import { CustomLink } from '@/components/ui/customLink/CustomLink';
 import ProjectsGallery from '@/components/projects/projectsGallery/ProjectsGallery';
 
@@ -16,10 +17,16 @@ export default function Page() {
                 <p className={styles['home__description']}>
                     Bienvenue sur mon portfolio&nbsp;! Je suis Chloé Thomas, développeuse web freelance passionnée par la création et le design. Je conçois des interfaces et solutions web qui répondent aux besoins des utilisateurs. Découvrez mes projets et explorons ensemble toutes les possibilités de donner vie à vos idées.
                 </p>
-                <CustomLink href="/about" className={`link hover-underline ${styles['home__link']}`}>
-                    <MdArrowOutward />
-                    En savoir plus
-                </CustomLink>
+                <div className={styles['home__infos']}>
+                    <div className={styles['home__infos__location']}>
+                        <TfiWorld />
+                        Basée à Vannes
+                    </div>
+                    <CustomLink href="/about" className="link hover-underline">
+                        <MdArrowOutward />
+                        En savoir plus
+                    </CustomLink>
+                </div>
             </section>
             <section className={styles['home__projects']}>
                 <ProjectsGallery />
