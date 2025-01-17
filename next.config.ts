b17d9e727fd32bced192f6ca18a27ca6a609b197
ChloeThomas <chloe.thomas.92@gmail.com>
2025-01-17 11:41:05 +0100

    images optimization

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
     reactStrictMode: true,
     sassOptions: {
         silenceDeprecations: ['legacy-js-api'],
-    }
+    },
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        deviceSizes: [640, 768, 1024, 1280, 1920],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    },
 };
 
 export default nextConfig;
