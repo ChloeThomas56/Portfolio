502aad202fdddd052b27748d20cffb9511cfd467
ChloeThomas <chloe.thomas.92@gmail.com>
2025-01-23 15:31:27 +0100

    language context en cours

---
+++ src/context/LanguageContext.tsx
@@ -0,0 +1,6 @@
+import { createContext, useContext } from 'react';
+import { useRouter } from 'next/router';
+// import translation files
+import fr from '../../locales/fr/home';
+import en from '../../locales/en/home';
+
