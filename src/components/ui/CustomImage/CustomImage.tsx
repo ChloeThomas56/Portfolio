bc2189cc01b6ba41f54a58bf8f7d0eebdba68a95
ChloeThomas <chloe.thomas.92@gmail.com>
2025-01-28 10:25:29 +0100

    remove img-container

--- src/components/ui/CustomImage/CustomImage.tsx
+++ src/components/ui/CustomImage/CustomImage.tsx
@@ -5,13 +5,11 @@ export default function CustomImage({ alt, ...props }: ImageProps) {
     const [isLoaded, setLoaded] = useState(false);
 
     return (
-        <div className="img-container">
-            <Image
-                {...props}
-                alt={alt}
-                style={{opacity: isLoaded ? 1 : 0}}
-                onLoad={() => setLoaded(true)}
-            />
-        </div>
+        <Image
+            {...props}
+            alt={alt}
+            style={{opacity: isLoaded ? 1 : 0}}
+            onLoad={() => setLoaded(true)}
+        />
     )
 }
