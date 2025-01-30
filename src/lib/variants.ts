313cb4b83e2fd6e9fd528979fe25c049a2cdb7dc
ChloeThomas <chloe.thomas.92@gmail.com>
2025-01-30 20:20:53 +0100

    fix mobile menu transition

--- src/lib/variants.ts
+++ src/lib/variants.ts
@@ -61,7 +61,7 @@ export const mobileMenuFade: Variants = {
     initial: { opacity: 0 },
     enter: {
         opacity: 1,
-        transition: { duration: 0.8 }
+        transition: { duration: 0.5 }
     },
     exit: (custom) => ({
         opacity: 0,
